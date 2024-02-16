dd146b459219e7d5858ad776b55ffadec93c8792
jioh19 <jioh1982@gmail.com>
2024-02-16 10:03:44 -0300

    fix bug

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AccountModule } from './application/account/account.module';
+import { UserModule } from './application/user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://jioh19:jioh19@cluster0.3jxsbjs.mongodb.net/?retryWrites=true&w=majority',
     ),
-    AccountModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
