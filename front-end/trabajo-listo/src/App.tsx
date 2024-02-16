35f77b7cedab6eeb15827429ffe017a449a352c9
Harry <harryyanarico19@gmail.com>
2024-02-15 21:49:32 -0400

    testimonios

--- front-end/trabajo-listo/src/App.tsx
+++ front-end/trabajo-listo/src/App.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "./containers/Navbar";
 import "./normalize.css";
 import { SearchPage } from "./pages/Search";
 import { Home } from "./pages/Home";
+import Testimonios from "./pages/Testimonios";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="search" element={<SearchPage />} />
+        <Route path="testimonios" element={<Testimonios />} />
       </Routes>
     </BrowserRouter>
   );
