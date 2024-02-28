ca64cb035395ed4c846be9bded1d4aa289155cde
Harry <harryyanarico19@gmail.com>
2024-02-28 10:49:05 -0400

    navbarResponsive

--- front-end/trabajo-listo/src/containers/Navbar.tsx
+++ front-end/trabajo-listo/src/containers/Navbar.tsx
@@ -16,6 +16,12 @@ import { UserState } from "@/components/component";
 import { userActions } from "@/store/userSlice";
 import { VerPerfilModal } from "@/modals/VerPerfilModal";
 
+import { useEffect, useState } from "react";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+} from "react-icons/ai";
+
 const listOfServices = [
   "Reparaciones",
   "Lavados",
@@ -48,25 +54,41 @@ export const Navbar = () => {
     dispatch(userActions.USER_LOGOUT());
   };
 
-  return (
-    <header className="relative font-libre-franklin">
-      <Link to="/">
-        <img
-          src={logo}
-          alt="logo Trabajo Listo"
-          className="top-2 left-2 absolute w-28"
-        />
-      </Link>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-      <nav className="flex items-center justify-end gap-12 bg-main-red px-20 h-16">
-        <NavigationMenu>
+  const [windowDimension, setWindowDimension] = useState({
+    innerHeight: window.innerHeight,
+    innerWidth: window.innerWidth,
+  });
+
+  useEffect(() => {
+    const detectSize = () => {
+      setWindowDimension({
+        innerHeight: window.innerHeight,
+        innerWidth: window.innerWidth,
+      });
+  
+      if (isMenuOpen && window.innerWidth >= 768) {
+        setIsMenuOpen(false);
+      }
+    };
+  
+    window.addEventListener('resize', detectSize);
+    return () => window.removeEventListener('resize', detectSize);
+  }, [isMenuOpen]);
+
+  const Content=(
+    <>
+      <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
               <NavigationMenuTrigger className="bg-transparent data-[state=open]:bg-white rounded-full text-base text-white data-[state=open]:text-black">
                 Servicios
               </NavigationMenuTrigger>
               <NavigationMenuContent>
-                <ul className="gap-1 grid md:grid-cols-3 p-4 w-[400px] md:w-[500px]">
+                <ul className={!isMenuOpen
+                  ?"gap-1 grid md:grid-cols-3 p-4 w-[400px] md:w-[500px]"
+                  :"gap-1 grid md:grid-cols-3 p-4 w-[300px] md:w-[500px]"}>
                   {listOfServices.map((service, i) => (
                     <li
                       className="hover:bg-gray-50 p-1 rounded-md font-semibold text-black hover:text-mbg-main-red cursor-pointer"
@@ -118,6 +140,46 @@ export const Navbar = () => {
             <FormSignUpModal />
           </>
         )}
+    </>
+  );
+
+  return (
+    <header className="relative font-libre-franklin">
+      <Link to="/">
+        <img
+          src={logo}
+          alt="logo Trabajo Listo"
+          className={!isMenuOpen && windowDimension.innerWidth > 435
+            ?"top-2 left-2 absolute w-28 z-50":
+            "top-2 left-2 absolute w-20 z-50"}
+        />
+      </Link>
+      <nav  className={!isMenuOpen
+        ?"flex items-center justify-end gap-5 bg-main-red px-10 h-16 flex-row mx-auto"
+        :"flex items-center justify-around gap-5 py-4 bg-main-red px-10  flex-col"}>
+        {windowDimension.innerWidth >= 768 ?(
+          <>
+            {Content}
+          </>
+          ):isMenuOpen &&(
+            <>
+            {Content}
+            </>
+            )  
+        }
+          {!isMenuOpen && windowDimension.innerWidth < 768 ? (<AiOutlineMenu 
+            cursor={"pointer"}
+            size={24}
+            color="#F2F2F2"
+            onClick={() => setIsMenuOpen(true)} />
+          ):windowDimension.innerWidth < 768 && (<AiOutlineClose 
+            cursor={"pointer"}
+            size={24}
+            color="#F2F2F2"
+            onClick={() => setIsMenuOpen(false)}
+            />
+          )
+        }
       </nav>
     </header>
   );
