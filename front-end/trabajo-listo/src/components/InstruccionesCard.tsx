74f786e0a910d7a08a002a89af1bb5c073a5bf06
Harry <harryyanarico19@gmail.com>
2024-03-01 17:00:55 -0400

    instrucciones Responsive

--- front-end/trabajo-listo/src/components/InstruccionesCard.tsx
+++ front-end/trabajo-listo/src/components/InstruccionesCard.tsx
@@ -5,16 +5,16 @@ import { FormSignUpModal } from "@/modals/FormSignUpModal";
 const InstruccionesCard = ({ instruccion }: { instruccion: Instruccion }) => {
   return (
     <article className={cn(
-      "flex items-center justify-around w-full font-libre-franklin",
-      instruccion.id === 2 && "flex-row-reverse"
+      "mb-4 flex items-center flex-col sm:flex-col md:flex-row md:gap-4 justify-around w-full font-libre-franklin",
+      instruccion.id === 2 && "flex-auto md:flex-row-reverse "
     )}>
-      <div className="shadow-xl rounded-xl w-2/5 h-auto overflow-hidden">
+      <div className="mb-4 flex shadow-xl rounded-xl w-4/5 sm:w-2/5 h-auto overflow-hidden">
         <img
           src={instruccion.imgUrl}
           alt={instruccion.title}
         />
       </div>
-      <div className="flex flex-col gap-4 bg-white p-8 border-2 border-dashed border-gray-700 rounded-xl w-1/2">
+      <div className="gap-4 bg-white p-8 border-2 border-dashed border-gray-700 rounded-xl w-full sm:w-1/2">
         <h2 className="font-bold text-3xl uppercase">
           <span className="mr-2 font-bold text-3xl text-main-red">
             {instruccion.id}.
