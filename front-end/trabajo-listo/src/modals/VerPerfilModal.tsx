3f210574b1d3d6c7d1cd3ed14fac0ee5654bdb93
ServiciosFA <fernando.salli.08@gmail.com>
2024-03-01 12:33:35 -0300

    Consultas respuestas

--- front-end/trabajo-listo/src/modals/VerPerfilModal.tsx
+++ front-end/trabajo-listo/src/modals/VerPerfilModal.tsx
@@ -19,47 +19,49 @@ import { notificacionesActions } from "@/store/notificacionesSlice";
 export const VerPerfilModal = ({ className }: { className?: string }) => {
   const user = useSelector((state: { user: UserState }) => state.user);
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const logoutHandler = () => {
     dispatch(userActions.USER_LOGOUT());
-    dispatch(notificacionesActions.SUCCES({
-      message: "Sesión cerrada"
-    }))
-  }
+    dispatch(
+      notificacionesActions.SUCCES({
+        message: "Sesión cerrada",
+      })
+    );
+  };
 
   interface opciones {
-    icon: JSX.Element
-    title: string
-    to: string
+    icon: JSX.Element;
+    title: string;
+    to: string;
   }
 
   const handleButton = (opcion: opciones) => {
-    navigate(opcion.to)
-  }
+    navigate(opcion.to);
+  };
 
   const opcionesPerfil = [
     {
       icon: <Bell className="w-5 h-5" />,
       title: "Notificaciones",
-      to: "/"
+      to: "/",
     },
     {
       icon: <MenuSquare className="w-5 h-5" />,
       title: "Ver perfil",
-      to: `perfil/${user.id}`
+      to: `perfil/${user.id}`,
     },
     {
       icon: <Pencil className="w-5 h-5" />,
       title: "Editar perfil",
-      to: "editar-perfil"
+      to: "editar-perfil",
     },
     {
       icon: <Star className="w-5 h-5" />,
       title: "Favoritos",
-      to: "/"
-    }
-  ]
+      to: "/",
+    },
+  ];
 
   return (
     <Sheet>
@@ -76,32 +78,31 @@ export const VerPerfilModal = ({ className }: { className?: string }) => {
       </SheetTrigger>
       <SheetContent className="flex flex-col w-72 font-libre-franklin">
         <SheetHeader className="mx-auto">
-          <SheetTitle className="text-gray-700">
-            Mi Perfil
-          </SheetTitle>
+          <SheetTitle className="text-gray-700">Mi Perfil</SheetTitle>
         </SheetHeader>
         <img
           className="mx-auto rounded-full w-20 h-20"
           src={user.imageProfile}
           alt="Foto de perfil"
         />
-        <span className="font-medium text-center">
-          {user.name}
-        </span>
+        <span className="font-medium text-center">{user.name}</span>
         <Separator className="" />
         <ul className="flex flex-col gap-2">
           {opcionesPerfil.map((opcion) => (
-            <SheetClose className="data-[state=open]:bg-secondary">
+            <SheetClose
+              key={opcion.title}
+              className="data-[state=open]:bg-secondary"
+            >
               <button
-                  key={opcion.title}
-                  onClick={() => handleButton(opcion)}
-                  className="flex items-center gap-2 hover:bg-gray-50 py-2 font-medium hover:text-main-red"
-                  >
-                    {opcion.icon}
-                    {opcion.title}
+                key={opcion.title}
+                onClick={() => handleButton(opcion)}
+                className="flex items-center gap-2 hover:bg-gray-50 py-2 font-medium hover:text-main-red"
+              >
+                {opcion.icon}
+                {opcion.title}
               </button>
               <span className="sr-only">Close</span>
-            </SheetClose>   
+            </SheetClose>
           ))}
           <li
             className="flex items-center gap-2 hover:bg-gray-100 py-2 font-medium hover:text-main-red hover:cursor-pointer"
